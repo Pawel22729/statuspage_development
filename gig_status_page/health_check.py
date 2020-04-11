79e6dc36098f661c28b20511c3ee4f74c29e3c36
Pablo Gonzalez <pablogonzalez@Pablos-MacBook-Air.local>
2020-04-11 17:09:59 +0200

    Changes

--- gig_status_page/health_check.py
+++ gig_status_page/health_check.py
@@ -3,6 +3,10 @@
 import requests
 import yaml
 import json
+import logging
+
+logger = logging.getLogger('my_healthcheck_app')
+logger.setLevel(logging.DEBUG)
 
 class Check():
     def __init__(self, *args, **kwargs):
@@ -12,19 +16,39 @@ class Check():
         for c in config['endpoints'][self.endpoint]:
             setattr(self, str(c), str(config['endpoints'][self.endpoint][c]))
 
-    def make_check(self):
+    def check_single(self):
         """Perform check and assert results based on configuration."""
-        is_code_ok, is_string_ok = False, False
+        is_alive = {
+            'endpoint_name': self.endpoint,
+            'is_alive': 'no'
+        }
         self.check = json.loads(self.check.replace("'", "\""))
         if self.check['type'] == 'http':
             req = requests.get(self.check['endpoint_url'])
             if self.check['expect_codes']:
                 if req.status_code in self.check['expect_codes']:
-                    is_code_ok = True
+                    is_alive['is_alive'] = 'yes'
+                else:
+                    is_alive['is_alive'] = 'no'  
             
             if self.check['expect_strings']:
                 for expect_string in self.check['expect_strings']:
                     if expect_string in str(req.content):
-                       is_string_ok = True
+                        is_alive['is_alive'] = 'yes'
+                    else:
+                        is_alive['is_alive'] = 'no'
 
-        return (is_code_ok and is_string_ok)
+        elif self.check['ping']:
+            pass                   
+
+        return json.dumps(is_alive)
+
+    
+def check_all():
+    results = []
+    with open('endpoints.yml', 'r') as f:
+        config = yaml.load(f, Loader=yaml.SafeLoader)
+    for e in config['endpoints']:
+        result = Check(endpoint=e).check_single()
+        results.append(result)
+    return results
--- gig_status_page/main.py
+++ gig_status_page/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import yaml
-from health_check import Check
+from health_check import Check, check_all
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -10,17 +10,16 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/status')
-def main_status():
-    endpoints_list = ['wp_endpoint', 'onet_endpoint']
+@app.route('/status/<endpoint>')
+def check_single(endpoint):
+    test_result = []
+    test_result.append(Check(endpoint=endpoint).check_single())
+    return render_template('index.html', results=test_result)
 
-    live_report = []
-    for endpoint in endpoints_list:
-        print(endpoint)
-        test_result = Check(endpoint=endpoint).make_check()
-        live_report.append( { 'name': endpoint, 'result': test_result } )
-        
-    return render_template('index.html', results=live_report) 
+@app.route('/all')
+def check_multi():
+    test_result = check_all()
+    return render_template('index.html', results=test_result) 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
